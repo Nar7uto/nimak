c357e7ba51e74ba428103ea5429799311508feac
Nima karimi <nima.karimi@live.com>
2018-03-28 22:04:46 +0430

    Design intro and service

--- nimak/settings.py
+++ nimak/settings.py
@@ -76,6 +76,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # processor for media
+                'django.template.context_processors.media',
             ],
         },
     },
