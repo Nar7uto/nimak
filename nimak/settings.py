4cfb003fabb96fe420cd0294fb1982675b677cc6
Nima Karimi <nima.karimi@live.com>
2018-04-23 18:12:38 +0430

    DELETE UR post | Tempory

--- nimak/settings.py
+++ nimak/settings.py
@@ -133,6 +133,7 @@ STATICFILES_FINDERS = (
 
 COMPRESS_ENABLED = True
 COMPRESS_CSS_FILTERS = ['compressor.filters.css_default.CssAbsoluteFilter',  'compressor.filters.cssmin.CSSMinFilter']
+
 # Media Files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
@@ -141,10 +142,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 GOOGLE_ANALYTICS_PROPERTY_ID = 'UA-116539833-1'
 
 # SSL Config
-
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE= True
-
-
