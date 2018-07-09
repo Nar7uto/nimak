9ec8d142ca17ebf8613f5be143002e911944f1d8
Nima Karimi <nima.karimi@live.com>
2018-07-10 01:15:13 +0430

    Fix | ReDesign

--- nimak/settings/prod.py
+++ nimak/settings/prod.py
@@ -1,5 +1,6 @@
 from .base import *
 
+DEBUG = False
 
 COMPRESS_ENABLED = True
 COMPRESS_CSS_FILTERS = ['compressor.filters.css_default.CssAbsoluteFilter',  'compressor.filters.cssmin.CSSMinFilter']
@@ -9,7 +10,7 @@ COMPRESS_CSS_FILTERS = ['compressor.filters.css_default.CssAbsoluteFilter',  'co
 GOOGLE_ANALYTICS_PROPERTY_ID = 'UA-116539833-1'
 
 # # SSL Config
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SECURE_SSL_REDIRECT = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE= True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE= True
