f9761935e43afe5f0497284dcc91d4f8688a1e43
Nima Karimi <nima.karimi@live.com>
2018-07-10 17:50:08 +0430

    Add | Gzip middleware

--- nimak/settings/base.py
+++ nimak/settings/base.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
 ]
 
 ROOT_URLCONF = 'nimak.urls'
